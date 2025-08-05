8a8e3b9bdd599bb04985c1572bbf947c3e1d8c1a
veerender nemali <nemaliveerender2001@gmail.com>
2025-08-05 17:50:11 +0530

    practiced CRUD operations get,post,patch,delete

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(express.json()) //this will convert incoming data which is in json format into js object and handsover to req.body
 
+//storing data in DB from user
 app.post("/signup", async (req, res) => {
     //creating a new instance of a user model
     const user = new User(req.body) //req.body => has js object
@@ -18,20 +19,51 @@ app.post("/signup", async (req, res) => {
     }
 })
 
-app.get("/user",async (req,res)=>{
+//getting data from DB
+app.get("/user", async (req, res) => {
     const userEmail = req.body.emailId  //request has come from the user
 
     try {
-        const user = await User.find({emailId : userEmail})
-        if(user.length === 0){
+        const user = await User.find({ emailId: userEmail }) //it works based on User schema model,if emailId is in user model then it can be found in DB
+        if (user.length === 0) {
             res.status(400).send("User not found!!")
-        }else{
+        } else {
             res.send(user)
         }
     } catch (error) {
         res.status(400).send("Something went wrong!")
     }
 
+})
+
+//updating user data 
+app.patch("/user", async (req, res) => {
+    const data = req.body
+    const userId = data.userId
+
+    try {
+        const user = await User.findByIdAndUpdate({ _id: userId }, data)
+        // console.log(user)
+        // if (!user) {
+        //     res.status(400).send("User not found!!")
+        // } else {
+        res.send("Successfully updated the user Data")
+        // }
+
+    } catch (error) {
+        res.status(400).send("Something went wrong while updating user data to DB")
+    }
+})
+
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId
+
+    try {
+        const user = await User.findByIdAndDelete({ _id: userId })
+        res.send("successfully deleted user from DB")
+    } catch (error) {
+        res.status(400).send("Something went wrong while deleting user data from DB")
+    }
 })
 
 //getting all data from DB
