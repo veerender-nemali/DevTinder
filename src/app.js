5ced93a7fc4dd11bbed4c439aaab489c9a46f2cd
veerender nemali <nemaliveerender2001@gmail.com>
2025-08-25 17:29:07 +0530

    encrypting password

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const connectDB = require("./config/database.js")
 const User = require("./models/user.js")
+const { validateSignUpData } = require('./utils/validation.js')
+const bcrypt = require("bcrypt")
 
 const app = express()
 
@@ -8,14 +10,44 @@ app.use(express.json()) //this will convert incoming data which is in json forma
 
 //storing data in DB from user
 app.post("/signup", async (req, res) => {
-    //creating a new instance of a user model
-    const user = new User(req.body) //req.body => has js object
-
     try {
+        //validation
+        validateSignUpData(req)
+
+        const { firstName, lastName, emailId, password } = req.body
+
+        //encrypting password
+        const passwordHash = await bcrypt.hash(password, 10)
+
+        //creating a new instance of a user model
+        const user = new User({ firstName, lastName, emailId, password: passwordHash }) //req.body => has js object
+
+
         await user.save()
         res.send("User data saved successfully")
     } catch (err) {
-        res.status(400).send("Error saving user data to DB" + err.message)
+        res.status(400).send("Error : " + err.message)
+    }
+})
+
+//login
+app.post('/login', async (req, res) => {
+    try {
+        const { emailId, password } = req.body
+
+        const user = await User.findOne({ emailId: emailId })
+        if (!user) {
+            throw new Error("Invalid credentials")
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if (isPasswordValid) {
+            res.send("Login successful!")
+        } else {
+            throw new Error("Invalid credentials")
+        }
+    } catch (error) {
+        res.status(400).send("Error : " + error.message)
     }
 })
 
@@ -41,8 +73,17 @@ app.patch("/user", async (req, res) => {
     const data = req.body
     const userId = data.userId
 
+    const ALLOWED_UPDATES = ['userId', 'skills', 'photoUrl', 'about']
+
     try {
-        const user = await User.findByIdAndUpdate({ _id: userId }, data)
+
+        const isUpdateAllowed = Object.keys(data).every(k => ALLOWED_UPDATES.includes(k))
+
+        if (!isUpdateAllowed) {
+            res.status(400).send("Update is not allowed")
+        }
+
+        const user = await User.findByIdAndUpdate({ _id: userId }, data, { runValidators: true }) //runvalidators will allow validate fun to run everytime when user updates data.
         // console.log(user)
         // if (!user) {
         //     res.status(400).send("User not found!!")
@@ -51,7 +92,7 @@ app.patch("/user", async (req, res) => {
         // }
 
     } catch (error) {
-        res.status(400).send("Something went wrong while updating user data to DB")
+        res.status(400).send("Something went wrong while updating user data to DB" + error.message)
     }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,76 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const validator = require("validator")
 
-const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            minLength: 4,
+            maxLength: 50
+        },
+        lastName: {
+            type: String,
+            minLength: 4,
+            maxLength: 50
+        },
+        password: {
+            type: String,
+            validate(val) {
+                if (!validator.isStrongPassword(val)) {
+                    throw new Error("password is not strong : " + value)
+                }
+            }
+        },
+        age: {
+            type: Number,
+            required: true,
+            min: 18
+        },
+        emailId: {
+            type: String,
+            required: true,
+            lowercase: true,
+            trim: true,
+            validate(val) {
+                if (!validator.isEmail(val)) {
+                    throw new Error("Invalid email Id : " + value)
+                }
+            }
+        },
+        gender: {
+            type: String,
+            required: true,
+            //this validate method only runs when a new document is created not when there is already a document present
+            validate(val) {
+                if (!["male", "female", "others"].includes(val)) {
+                    throw new Error("Gender data is not valid")
+                }
+            }
+        },
+        photoUrl: {
+            type: String,
+            default: "https://veerimg.com",
+            validate(val) {
+                if (!validator.isURL(val)) {
+                    throw new Error("Invalid Url Id : " + value)
+                }
+            }
+        },
+        about: {
+            type: String,
+            default: "This is about default user"
+        },
+        skills: {
+            type: [String],
+            max: 10
+        }
     },
-    lastName: {
-        type: String
-    },
-    age: {
-        type: Number
-    },
-    firstName: {
-        type: String
-    },
-    emailId: {
-        type: String
-    },
-    gender: {
-        type: String
+    {
+        timestamps: true,
     }
-})
+);
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
