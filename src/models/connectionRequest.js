b79b54bb979853dc2dc08cbb46a09471c4229233
veerender nemali <nemaliveerender2001@gmail.com>
2025-08-29 02:43:35 +0530

    Logical DB query and Compound Indexing and created /request/send/:status/:userId api

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+const connectionRequestSchema = new mongoose.Schema(
+    {
+        fromUserId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true
+        },
+        toUserId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true,
+            enum: {
+                values: ["interested", "ignored", "accepted", "rejected"],
+                message: `{VALUE} is incorrect status type`
+            }
+        }
+    },
+    { timestamps: true }
+);
+
+connectionRequestSchema.index({ firstName: 1, lastName: 1 })
+
+//this is a middleware, it gets executed before data is being stored in DB
+connectionRequestSchema.pre("save", function () {
+    const connectionRequest = this
+
+    if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+        throw new Error("Connot send connection request to yourseld!")
+    }
+
+    next()
+})
+
+const ConnectionRequestModel = new mongoose("ConnectionRequest", connectionRequestSchema)
+
+module.exports = ConnectionRequestModel
--- src/routes/request.js
+++ src/routes/request.js
@@ -2,10 +2,55 @@ const express = require("express")
 const requestRouter = express.Router()
 
 const { userAuth } = require("../middlewares/auth.js")
+const ConnectionRequest = require("../models/connectionRequest.js")
 
-requestRouter.post("/request/send/interested/:userId", userAuth, async (req, res) => {
+requestRouter.post("/request/send/:status/:userId", userAuth, async (req, res) => {
     try {
-        res.send("connection request sent!!")
+        const fromUserId = req.user._id
+        const toUserId = req.params.userId
+        const status = req.params.status
+
+        //corner-case : 1
+        const allowedStatuses = ["interested", "ignored"]
+        if (!allowedStatuses.includes(status)) {
+            return res.status(400).json({
+                message: "Invalid status type : " + status
+            })
+        }
+
+        //corner-case : 2
+        const isUserAvailableInDB = await ConnectionRequest.findById({ toUserId })
+        if (!isUserAvailableInDB) {
+            return res.status(400).json({
+                message: "User is not available"
+            })
+        }
+
+        //corner-case : 3 & 4 combined
+        const existingConnectionRequest = await ConnectionRequest.findOne({
+            $or: [
+                { fromUserId, toUserId },
+                { fromUserId: toUserId, toUserId: fromUserId }
+            ]
+        })
+        if (existingConnectionRequest) {
+            return res.status(400).json({
+                message: "Connection request already exists!"
+            })
+        }
+
+        const connectionRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status
+        })
+
+        const data = await connectionRequest.save()
+
+        res.json({
+            message: status === "interested" ? "Request sent succesfully!!" : "Ignored succesffuly!!",
+            data
+        })
     } catch (error) {
         res.status(400).send("Error : " + error.message)
     }
