0e16468a1071dbd02e773d491596ad64d4887929
veerender nemali <nemaliveerender2001@gmail.com>
2025-09-06 00:38:59 +0530

    user apis

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router()
 
 const { userAuth } = require("../middlewares/auth")
 const ConnectionRequest = require("../models/connectionRequest")
+const User = require("../models/user")
 
 userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
     try {
@@ -54,5 +55,40 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     }
 })
 
+userRouter.get("/feed", userAuth, async (req, res) => {
+    try {
+
+        const loggedInUser = req.user
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ]
+        }).select("fromUserId toUserId")
+
+        let hideUsersFromFeed = new Set()
+
+        connectionRequests.forEach(element => {
+            hideUsersFromFeed.add(element._id)
+        });
+
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(connectionRequests) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select("firstName secondName")
+
+        res.json({
+            message: "User Feed!",
+            data: users
+        })
+
+
+    } catch (error) {
+        res.status(400).send("Error : " + error.messsage)
+    }
+})
 
 module.exports = userRouter
